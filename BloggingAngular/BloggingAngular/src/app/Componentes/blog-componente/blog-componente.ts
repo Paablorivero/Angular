28347785ec1d703f2d41654c7314d875a62f0780
COMPU-S038\daw2 <pablo.rivero.gutierrez@gmail.com>
2025-11-06 18:33:44 +0100

    comienzo proyecto Blogging angular

---
+++ BloggingAngular/BloggingAngular/src/app/Componentes/blog-componente/blog-componente.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { noticiaInterfaz } from '../../interfaces/noticiaInterfaz';
+
+@Component({
+  selector: 'app-blog-componente',
+  imports: [],
+  templateUrl: './blog-componente.html',
+  styleUrl: './blog-componente.css',
+})
+export class BlogComponente {
+
+  @Input() noticia: noticiaInterfaz
+
+  constructor(){
+    this.noticia = {
+      nombre: "Predeterminado",
+      imagen: "Predetermminado",
+      noticia: "noticia",
+      fecha: "fecha"
+    };
+  }
+
+  getnombre()
+}
---
+++ BloggingAngular/BloggingAngular/src/app/interfaces/noticiaInterfaz.ts
@@ -0,0 +1,8 @@
+
+export interface noticiaInterfaz {
+
+        nombre: string,
+        imagen: string,
+        noticia: string,
+        fecha: string
+}
